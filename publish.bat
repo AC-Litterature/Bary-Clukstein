84f59de806b467119784f5274501088e124b2bc0
Adrien <adrien.castex@protonmail.com>
2022-08-04 15:30:16 +0200

    Added auto-push in publish file

--- publish.bat
+++ publish.bat
@@ -1,3 +1,3 @@
 @echo off
 
-npm i && npm run build && git add -A && git commit -m "modification" && git push heroku master && pause
+npm i && npm run build && git add -A && git commit -m "modification" && git push origin master && git push heroku master && pause
