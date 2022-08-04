247b3f9146fa262c38688bea71b9e51ecb215d80
Adrien <adrien.castex@protonmail.com>
2022-08-04 14:57:26 +0200

    Fixed publish file

--- publish.bat
+++ publish.bat
@@ -1,10 +1,3 @@
 @echo off
 
-npm i
-npm run build
-
-git add -A
-git commit -m "modification"
-git push heroku master
-
-pause
+npm i && npm run build && git add -A && git commit -m "modification" && git push heroku master && pause
